6871d5f4a7beaa92af63be44597401349cf57cbc
Brian Juma <bjuma0985@gmail.com>
2025-05-02 12:56:31 +0300

    java practise

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,37 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-//import java.util.Scanner;
+import java.util.Scanner;
 public class Main {
     public static void main(String[]args){
         //compound interest calculator
 
+        Scanner scanner = new Scanner(System.in);
+
+        double principal;
+        double rate;
+        int timesCompound;
+        int years;
+        double amount;
+
+        System.out.println("Enter the principal amount: ");
+        principal = scanner.nextDouble();
+
+        System.out.println("Enter the interest rate (in %): ");
+        rate = scanner.nextDouble() /100;
+
+        System.out.println("Enter the number of times compounded per year: ");
+        timesCompound = scanner.nextInt();
+
+        System.out.println("Enter the number of years: ");
+        years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + rate / timesCompound, timesCompound * years);
+
+        System.out.printf("The amount after %d years is %.2f", years, amount );
+
+
+
+        scanner.close();
     }
 }
 
